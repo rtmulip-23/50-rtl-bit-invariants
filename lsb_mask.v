22607132b7e4450cf0794c2650516c5ad621d80c
raviteja-1369 <l82341560@gmail.com>
2026-02-01 22:14:27 +0530

    Solved Lowest Set Bit Mask (Challenge #6)

---
+++ lsb_mask.v
@@ -0,0 +1,13 @@
+// Problem 6: Lowest Set Bit Mask
+// Generate a mask with only the lowest set bit of 'in' set. (e.g., 0010100 -> 0000100)
+
+module lsb_mask (
+    input [31:0] in,
+    output [31:0] mask
+);
+
+    // Your code here
+
+    assign mask = (in&((~in + 1)));
+
+endmodule
